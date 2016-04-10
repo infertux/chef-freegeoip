cc71eb716f9f51538d91c660abcf8c41713934e7
Cédric Félizard <cedric@felizard.fr>
2016-04-10 09:26:33 +0000

    Update freegeoip to latest release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,12 @@
 CHANGELOG
 =========
 
+## v1.1.0
+- Update freegeoip to latest release
+
 ## v1.0.1
 - Handle binary updates properly
 
 ## v1.0.0
-- Initial release.
+- Initial release
 
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['freegeoip']['user'] = 'freegeoip'
-default['freegeoip']['version'] = '3.0.10'
+default['freegeoip']['version'] = '3.1.4'
 default['freegeoip']['archive'] = "freegeoip-#{node['freegeoip']['version']}-linux-amd64.tar.gz"
 default['freegeoip']['url'] = "https://github.com/fiorix/freegeoip/releases/download/v#{node['freegeoip']['version']}/#{node['freegeoip']['archive']}"
-default['freegeoip']['checksum'] = '30b828faee350519c465d35fed36e0906f7c4e7af15f4501d709e9be882df027'
+default['freegeoip']['checksum'] = '50005da7f0a24a0e5873e9a0f12a8fbe0a3bbaaaffb9e7820ebc94b35dd58f85'
 default['freegeoip']['binary'] = '/usr/local/bin/freegeoip'
 default['freegeoip']['arguments'] = '-http 127.0.0.1:9999 -use-x-forwarded-for'
 default['freegeoip']['service_wrapper'] = 'freegeoip'
