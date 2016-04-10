33ada3dc7756a1f7c7c5788904591073a68ae4ee
Cédric Félizard <cedric@felizard.fr>
2016-04-10 09:38:35 +0000

    Bump version to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs freegeoip software from freegeoip.net'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/infertux/chef-freegeoip'
 issues_url       'https://github.com/infertux/chef-freegeoip/issues'
-version          '1.0.1'
+version          '1.1.0'
 
 supports 'debian'
